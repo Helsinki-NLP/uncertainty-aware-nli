c1d33bba2ce0720a0f1eed7f28741956a7e3aaf3
aarne.talman@helsinki.fi <aarne.talman@basement.ai>
2022-05-11 10:04:33 +0300

    fix data type of lables

--- data.py
+++ data.py
@@ -20,7 +20,7 @@ class NLIDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item["labels"] = torch.tensor(self.labels[idx])
+        item["labels"] = torch.tensor(int(self.labels[idx]))
         return item
 
     def __len__(self):
