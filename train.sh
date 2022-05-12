43d0f76bcb3d078d7a8d8f5781d22f71ce8ff1cd
aarne.talman@helsinki.fi <aarne.talman@basement.ai>
2022-05-12 09:09:45 +0300

    changed training config from puhti - > mahti

--- train.sh
+++ train.sh
@@ -2,11 +2,10 @@
 #SBATCH -J MNLI
 #SBATCH -o out_%J.txt
 #SBATCH -e err_%J.txt
-#SBATCH -p gpu
+#SBATCH -p gpusmall
 #SBATCH -n 1
-#SBATCH -t 10:00:00
-#SBATCH --mem-per-cpu=64000
-#SBATCH --gres=gpu:v100:1
+#SBATCH -t 72:00:00
+#SBATCH --gres=gpu:a100:1
 #SBATCH --account=project_2001194
 # run command
 
