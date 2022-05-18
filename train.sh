4cd93587c1fc6dc8ad43f7965e7f77a7116e3c76
aarne.talman@helsinki.fi <aarne.talman@basement.ai>
2022-05-18 15:22:20 +0300

    fixed train.sh

--- train.sh
+++ train.sh
@@ -5,6 +5,7 @@
 #SBATCH -p gpu
 #SBATCH -n 1
 #SBATCH -t 72:00:00
+#SBATCH --mem-per-cpu=64000
 #SBATCH --gres=gpu:v100:1
 #SBATCH --account=project_2001194
 # run command
@@ -19,6 +20,7 @@ srun python main.py \
     --model roberta \
     --batch_size 32 \
     --epochs 5 \
+    --optimizer ${OPTIMIZER} \
     --gpu 0 \
     --method ${METHOD} \
     --dataset ${DATASET}
